7a93434d7bd8227b02177ef2b13c3126ed3922eb
[jacksonkgezane] <[jacksonkgezane@gmail.com]>
2022-01-27 11:01:40 +0200

    Update project.java
    Creating public class and main class for my project

---
+++ project.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        int x;
+        String FirstName;
+        int y = 5;
+        String S = "Hi";
+
+        Scanner sc = new Scanner(System.in);        //Scanner statement
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
