04787f8addadb103f8aad1967987f4ee1f5d1adc
[jacksonkgezane] <[jacksonkgezane@gmail.com]>
2022-01-27 11:14:26 +0200

    Update project.java
    Adding the inputs and outputs of the of the project.

--- project.java
+++ project.java
@@ -10,6 +10,24 @@ class Main {
 
         Scanner sc = new Scanner(System.in);        //Scanner statement
 
+        System.out.println("Enter Name");
+        FirstName = sc.nextLine();               //Read user input
+
+        System.out.println( S+" "+ FirstName);
+
+
+        System.out.println("Enter first number");   //Tell user to enter first name
+        x = sc.nextInt();
+
+        
+
+       
+        System.out.println(x + y);
+
+       
+
+
+
 
 
 
